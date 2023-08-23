e2abf8ad121bc7b50b878e3126e920307e95d804
Bukeuw <ato@ukeuw.com>
2023-08-23 23:33:16 +0700

    Add panache entity for product category

---
+++ src/main/java/com/gia/grocery/shop/model/Category.java
@@ -0,0 +1,22 @@
+package com.gia.grocery.shop.model;
+
+import java.time.Instant;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "g_categories")
+public class Category extends PanacheEntity {
+
+  @Column(length = 50, nullable = false)
+  public String title;
+
+  @Column(name = "created_at", nullable = false)
+  public Instant createdAt = Instant.now();
+
+  @Column(name = "updated_at", nullable = false)
+  public Instant updatedAt = Instant.now();
+}
