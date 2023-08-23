74ccaaecabc6b492caa5321c24cc29119dede3d1
Bukeuw <ato@ukeuw.com>
2023-08-23 13:55:04 +0700

    Add test for updating product

--- src/test/java/com/gia/grocery/shop/repository/ProductRepositoryTest.java
+++ src/test/java/com/gia/grocery/shop/repository/ProductRepositoryTest.java
@@ -2,6 +2,7 @@ package com.gia.grocery.shop.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
 
@@ -48,6 +49,52 @@ public class ProductRepositoryTest {
 
     Product.persist(product);
     assertNotNull(product.id);
+
+    product = Product.findById(product.id);
+    assertEquals(name, product.name);
+    assertEquals(imageUrl, product.imageUrl);
+    assertEquals(stock, product.stock);
+    assertEquals(price, product.price);
+    assertEquals(unit, product.unit);
+  }
+
+  @Test
+  @TestTransaction
+  public void shouldUpdateProduct() {
+    String name = "Carrot";
+    String imageUrl = "https://cnd.gia-grocery.com/img/products/carrot.webp";
+    int stock = 100;
+    BigDecimal price = new BigDecimal(20);
+    Unit unit = Unit.GRAM;
+
+    Product product = new Product();
+    product.name = name;
+    product.imageUrl = imageUrl;
+    product.stock = stock;
+    product.price = price;
+    product.unit = unit;
+
+    Product.persist(product);
+    assertNotNull(product.id);
+    assertEquals(name, product.name);
+    assertEquals(imageUrl, product.imageUrl);
+    assertEquals(stock, product.stock);
+    assertEquals(price, product.price);
+    assertEquals(unit, product.unit);
+
+    // update product
+    name = "Potato";
+    imageUrl = "https://cnd.gia-grocery.com/img/products/potato.webp";
+    stock = 1000;
+    price = new BigDecimal(10);
+    unit = Unit.GRAM;
+
+    product.name = name;
+    product.imageUrl = imageUrl;
+    product.stock = stock;
+    product.price = price;
+    product.unit = unit;
+    Product.persist(product);
 
     product = Product.findById(product.id);
     assertEquals(name, product.name);
