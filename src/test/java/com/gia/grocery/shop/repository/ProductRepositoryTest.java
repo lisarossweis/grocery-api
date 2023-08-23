d642da2409942f3983296278a94b0b69d9b691f3
Bukeuw <ato@ukeuw.com>
2023-08-23 13:14:53 +0700

    Add test for finding product

--- src/test/java/com/gia/grocery/shop/repository/ProductRepositoryTest.java
+++ src/test/java/com/gia/grocery/shop/repository/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.gia.grocery.shop.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.math.BigDecimal;
@@ -27,5 +28,32 @@ public class ProductRepositoryTest {
 
     Product.persist(product);
     assertNotNull(product.id);
+  }
+
+  @Test
+  @TestTransaction
+  public void shouldFindProduct() {
+    String name = "Carrot";
+    String imageUrl = "https://cnd.gia-grocery.com/img/products/carrot.webp";
+    int stock = 100;
+    BigDecimal price = new BigDecimal(20);
+    Unit unit = Unit.GRAM;
+
+    Product product = new Product();
+    product.name = name;
+    product.imageUrl = imageUrl;
+    product.stock = stock;
+    product.price = price;
+    product.unit = unit;
+
+    Product.persist(product);
+    assertNotNull(product.id);
+
+    product = Product.findById(product.id);
+    assertEquals(name, product.name);
+    assertEquals(imageUrl, product.imageUrl);
+    assertEquals(stock, product.stock);
+    assertEquals(price, product.price);
+    assertEquals(unit, product.unit);
   }
 }
